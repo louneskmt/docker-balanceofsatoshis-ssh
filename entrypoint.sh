9c428ac0b95bb0c72e9a870d613071dd88bab5fd
Lounès Ksouri <dev@louneskmt.com>
2021-12-04 10:40:49 +0100

    fix: use host instead of IP + minor fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# User configuration
 USER_NAME=${USER_NAME:-"umbrel"}
 USER_PASSWORD=${USER_PASSWORD:-"moneyprintergobrrr"}
 
+# Lightning and BoS configuration
 NETWORK=${NETWORK:-"mainnet"}
 LND_PATH=${LND_PATH:-"/lnd"}
 BOS_NODE_NAME=${BOS_NODE_NAME:-"umbrel"}
 BOS_NODE_PATH=${BOS_NODE_PATH:-"/home/${USER_NAME}/.bos/${BOS_NODE_NAME}"}
-export BOS_DEFAULT_SAVED_NODE=${BOS_NODE_NAME}
+echo "BOS_DEFAULT_SAVED_NODE=${BOS_NODE_NAME}" >> /etc/environment
 
 # Create user
 id -u ${USER_NAME} &>/dev/null || {
@@ -16,6 +18,9 @@ id -u ${USER_NAME} &>/dev/null || {
 	echo "${USER_NAME}:${USER_PASSWORD}" | chpasswd
 }
 
+# Save umbrel.local host (to avoid node warning)
+echo "${LND_IP} umbrel.local" >> /etc/hosts
+
 # Save node connection details
 mkdir -p ${BOS_NODE_PATH}
 
@@ -23,19 +28,23 @@ cat <<EOF > ${BOS_NODE_PATH}/credentials.json
 {
   "cert_path": "${LND_PATH}/tls.cert",
   "macaroon_path": "${LND_PATH}/data/chain/bitcoin/${NETWORK}/admin.macaroon",
-  "socket": "${LND_IP}:${LND_PORT}"
+  "socket": "umbrel.local:${LND_PORT}"
 }
 EOF
 
-chown -R 1000:1000 ${BOS_NODE_PATH}
+chown -R 1000:1000 "/home/${USER_NAME}"
 
 # Configure motd (SSH welcome message)
 cat <<EOF > /etc/motd
 
-#########################
-#        UMBREL         #
-#  Balance Of Satoshis  #
-#########################
+###################################
+#                                 #
+#             UMBREL              #
+#       Balance Of Satoshis       #
+#                                 #
+###################################
+
+Usage: bos <command>
 
 EOF
 
